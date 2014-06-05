82f1d0faec45c5c4a522a7657bc9bd1b744a6687
Ronald Klaus <ronald.klaus@gmx.net>
2014-06-05 07:57:28 +0200

    Add user and role.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   # attr_accessible :title, :firstname, :lastname, :gender, :description
+
+  has_many :roles
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140521205558) do
+ActiveRecord::Schema.define(version: 20140605055007) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20140521205558) do
   create_table "images", force: true do |t|
     t.string   "url"
     t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
